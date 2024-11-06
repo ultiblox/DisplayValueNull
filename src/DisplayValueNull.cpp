e0cdfb012e1dc31fac3dff5374b5ecc67b7c7deb
Maker Zone <dev@makerzone.com.au>
2024-11-06 12:25:46 +1100

    Fix

--- src/DisplayValueNull.cpp
+++ src/DisplayValueNull.cpp
@@ -1,19 +1,3 @@
 #include "DisplayValueNull.h"
 
-// All methods are empty implementations
-
-DisplayValueNull::DisplayValueNull() {}
-
-void DisplayValueNull::init() {}
-
-void DisplayValueNull::clear() {}
-
-void DisplayValueNull::setLabel(const char* value) {}
-
-void DisplayValueNull::setLabel(const __FlashStringHelper* value) {}
-
-void DisplayValueNull::setValue(const char* value) {}
-
-void DisplayValueNull::setValue(int value) {}
-
-void DisplayValueNull::setSuffix(const char* value) {}
+// Empty .cpp file because all methods are defined inline in the header
